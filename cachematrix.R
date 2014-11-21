6f6715ae9233ed567553ac81e900427bf7aa4b4d
ThoDuyNguyen <duythonguyen35@gmail.com>
2014-11-22 00:32:40 +0700

    Adding more comment

--- cachematrix.R
+++ cachematrix.R
@@ -12,28 +12,37 @@
 ## and it's inverse matrix (if the matrix is invertible).
 ## Initially, when a matrix is created, the inverse matrix is 
 ## not computed. Also if the data (original matrix) is changed, 
-## cache  
+## "cached" is cleared
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## This is the cache of inverse matrix
     cachedInverseMatrix <- NULL
     
+    ## Set data for the matrix
+    ## Since then, the "cache" inverse matrix of it also need
+    ## to be cleared
     setFunction <- function(y) {
         x <<- y
         cachedInverseMatrix <<- NULL
     }
     
+    ## Get the matrix 
     getFunction <- function() {
         x
     }
     
+    ## Set new value for the the cached inverse matrix
     setInverseFunction <- function(InverseMatrix) {
         cachedInverseMatrix <<- InverseMatrix
     }
     
+    ## Get value of inverse matrix
     getInverseFunction <- function() {
         cachedInverseMatrix
     }
     
+    ## Private methods is mapped to public so that
+    ## we could called them    
     list(set = setFunction, get = getFunction,
          setInverseMatrix = setInverseFunction,
          getInverseMatrix = getInverseFunction)
@@ -48,11 +57,17 @@ cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     InverseMatrix <- x$getInverseMatrix()
     
+    ## If the inverse matrix is cache, just return it,
+    ## there is no need to compute it again. 
+    ## A message is also printed out to notify user. 
     if(!is.null(InverseMatrix)) {
         message("getting cached data")
         return(InverseMatrix)
     }
     
+    ## The inverse matrix is not cached yet. 
+    ## Compute and then cache inverse matrix 
+    ## for further use
     data <- x$get()
     InverseMatrix <- solve(data, ...)
     x$setInverseMatrix(InverseMatrix)
